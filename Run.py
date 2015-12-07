d48caa238b133d8282a8f99329ce55a1e80e32f2
spencerkdesigns <contact.spartandesigns@gmail.com>
2015-12-07 16:49:53 -0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -9,7 +9,7 @@ readbuffer = ""
 
 while True:
 		readbuffer = readbuffer + s.recv(1024)
-		temp = string.split(readbuffer, "\n")
+		temp = string.split(readbuffer, "\r\n")
 		readbuffer = temp.pop()
 		
 		for line in temp:
@@ -23,4 +23,29 @@ while True:
 			if "You Suck" in message:
 				sendMessage(s, "No, you suck!")
 				break
+			if "!Join" in message:
+				sendMessage(s, "Here is where you apply https://freedom.tm/auth/creatorstudios+apply")
+				break
+				print(line)
+			if "!Twitter" in message:
+				sendMessage(s, "Follow us here: https://twitter.com/creatorstudios_")
+				break
+				print(line)
+			if "https://" in message:
+				sendMessage(s, "No links allowed")
+				break
+				print(line)
 			
+			if "http://" in message:
+				sendMessage(s, "No links allowed")
+				break
+				print(line)
+			
+			if ".com" in message:
+				sendMessage(s, "No links allowed")
+				break
+				print(line)
+			if "!Rules" in message:
+				sendMessage(s, "No links allowed!" " " "Be nice!" " " "Don't fight!" " " "Swearing should be at a minimum!")
+				break
+				print(line)
